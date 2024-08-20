5bed7cefea6020c5c6d6e9a21d62eebbf1dbc0d9
andregoobloo <177800443+andregoobloo@users.noreply.github.com>
2024-08-20 09:07:15 -0400

    Adds button to enter canvas size

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 "use strict";
 
 const container = document.querySelector(".container");
+const canvasSize = document.querySelector(".canvas");
 
-for (let i = 0; i < 16; i++) {
-  const column = document.createElement("div");
-  column.classList.add("column");
-  container.append(column);
-  for (let j = 0; j < 16; j++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    column.append(box);
+const createCanvas = function (size) {
+  for (let i = 0; i < size; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+    container.append(column);
+    for (let j = 0; j < size; j++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      column.append(box);
+    }
   }
-}
+};
+
+createCanvas(16);
+
 const box = document.querySelectorAll(".box");
 
 container.addEventListener("mouseover", function (e) {
@@ -19,4 +25,9 @@ container.addEventListener("mouseover", function (e) {
   if (e.target.className === "box") {
     e.target.style.backgroundColor = "pink";
   }
+});
+
+canvasSize.addEventListener("click", function (e) {
+  e.preventDefault();
+  const gridSize = Number(prompt("Enter number of squares per side"));
 });
